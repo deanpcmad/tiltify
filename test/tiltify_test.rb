2f095afc0726613e364853dd66e1e8e8387337f7
Dean Perry <dean@deanpcmad.com>
2021-10-24 21:52:07 +0100

    comment this test out

--- test/tiltify_test.rb
+++ test/tiltify_test.rb
@@ -7,7 +7,7 @@ class TiltifyTest < Minitest::Test
     refute_nil ::Tiltify::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
+  # def test_it_does_something_useful
+  #   assert false
+  # end
 end
