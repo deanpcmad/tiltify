bd5c38b5ed572e6bfb8a3900c4833fb14aebbab2
Dean Perry <deanperry@fastmail.com>
2022-04-26 21:14:28 +0100

    added created_at and updated_at to donations

--- lib/tiltify/objects/donation.rb
+++ lib/tiltify/objects/donation.rb
@@ -1,4 +1,12 @@
 module Tiltify
   class Donation < Object
+
+    def initialize(options = {})
+      super options
+
+      self.created_at = Time.at(options["completedAt"].to_s[0..-4].to_i)
+      self.updated_at = Time.at(options["updatedAt"].to_s[0..-4].to_i)
+    end
+
   end
 end
