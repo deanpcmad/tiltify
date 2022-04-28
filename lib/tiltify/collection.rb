6f32990bc8148e9e765480a1a73547ae209f5831
Dean Perry <deanperry@fastmail.com>
2022-04-28 16:07:32 +0100

    added has_prev and has_next

--- lib/tiltify/collection.rb
+++ lib/tiltify/collection.rb
@@ -1,19 +1,23 @@
 module Tiltify
   class Collection
-    attr_reader :data, :total
+    attr_reader :data, :total, :has_prev, :has_next
 
     def self.from_response(response, type:)
       body = response.body
 
       new(
         data: body["data"].map { |attrs| type.new(attrs) },
-        total: body["data"].count
+        total: body["data"].count,
+        has_prev: !body["links"]["prev"].blank?,
+        has_next: !body["links"]["next"].blank?
       )
     end
 
-    def initialize(data:, total:)
+    def initialize(data:, total:, has_prev:, has_next:)
       @data = data
       @total = total
+      @has_prev = has_prev
+      @has_next = has_next
     end
   end
 end
