5f2a175738afd3f420f7cdbfcf874faa99c11a0d
Dean Perry <deanperry@fastmail.com>
2022-04-28 15:46:08 +0100

    completed_at not created_at

--- lib/tiltify/objects/donation.rb
+++ lib/tiltify/objects/donation.rb
@@ -4,8 +4,8 @@ module Tiltify
     def initialize(options = {})
       super options
 
-      self.created_at = Time.at(options["completedAt"].to_s[0..-4].to_i)
-      self.updated_at = Time.at(options["updatedAt"].to_s[0..-4].to_i)
+      self.completed_at = Time.at(options["completedAt"].to_s[0..-4].to_i)
+      self.updated_at   = Time.at(options["updatedAt"].to_s[0..-4].to_i)
     end
 
   end
